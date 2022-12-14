3d2fe20dd2ed6ffa54746aaea0d83448e5e6c14b
Alefer Reinert <aleferreinert@gmail.com>
2022-12-14 11:47:55 -0300

    Finalizado rodapé responsivo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -48,51 +48,16 @@ export function Footer() {
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
-                        {/* <div className="column">
-                            <h2 data-toggle="collapse" className="collapsed" data-target="#list-about" data-parent=".site-map">
-                                Institucional
-                                <span className="fa fa-angle-down icon-collapsed"></span>
-                                <span className="fa fa-angle-up icon-show"></span>
-                            </h2>
-                            <ul className="list collapse" id="list-about">
-                                <li><a href="./">Lojas</a></li>
-                                <li><a href="./">Sobre nós</a></li>
-                                <li><a href="./">Trabalhe conosco</a></li>
-                                <li><a href="./">Blog</a></li>
-                            </ul>
-                        </div>
-                        <div className="column">
-                            <h2 data-toggle="collapse" className="collapsed" data-target="#list-support" data-parent=".site-map">
-                                Ajuda e suporte
-                                <span className="fa fa-angle-down icon-collapsed"></span>
-                                <span className="fa fa-angle-up icon-show"></span>
-                            </h2>
-                            <ul className="list collapse" id="list-support">
-                                <li><a href="/prazos-de-entrega">Prazos de entrega</a></li>
-                                <li><a href="/trocas-e-devolucoes">Trocas e devoluções</a></li>
-                                <li><a href="/perguntas-frequentes">Perguntas Frequentes</a></li>
-                                <li><a href="/politica-de-privacidade">Política de privacidade</a></li>
-                            </ul>
-                        </div>
-                        <div className="column">
-                            <h2 data-toggle="collapse" className="collapsed" data-target="#list-contact" data-parent=".site-map">
-                                Contato
-                                <span className="fa fa-angle-down icon-collapsed"></span>
-                                <span className="fa fa-angle-up icon-show"></span>
-                            </h2>
-                            <ul className="list collapse" id="list-contact">
-                                <li><a href="./">Tel.: (47) 55555-5555</a></li>
-                                <li><a href="./">Tel.2: (47) 55555-5555</a></li>
-                                <li><a href="./">E-mail: atendimento@loja.com.br</a></li>
-                            </ul>
-                        </div>*/}
-                        <div className="column column-social">
+                        <div className="column-social">
                             <h2>Social</h2>
                             <Social />
-                        </div> 
+                        </div>
                     </div>
                 </nav>
             </div>
+            <div className="bar-social">
+                <Social />
+            </div> 
             <div className="bar-middle">
                 <div className="container">
                     <ul className="payments">
