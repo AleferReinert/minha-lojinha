2376e93a03273b6a4d8180dcc0745e79da013117
Alefer Reinert <aleferreinert@gmail.com>
2022-12-28 23:28:14 -0300

    Corrigido url base para o github-pages

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
 import Cart from "./pages/Cart";
 import Favorites from './pages/Favorites';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="*" element={<NoPage />} />
-            <Route path="/carrinho" element={<Cart />} />
-            <Route path="/lista-de-desejos" element={<Favorites />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter basename='/react-commerce'>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="*" element={<NoPage />} />
+                    <Route path="/carrinho" element={<Cart />} />
+                    <Route path="/lista-de-desejos" element={<Favorites />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
-  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
